523f391dceb3199bf29748c1fbe510063ec9d34b
Thibaud Lambert <thibaud.lambert@gmail.com>
2013-10-01 11:27:01 +0200

    Update Arduino file

--- arduino/firstbot/firstbot.ino
+++ arduino/firstbot/firstbot.ino
@@ -3,6 +3,15 @@
 #define LEFT_DIRECTION 8
 #define LEFT_SPEED 10
 
+#define LEFT 0
+#define RIGHT 1
+
+#define SIZE_BUFFER 16
+char buffer[SIZE_BUFFER];
+int buffer_lenght;
+
+int rs, rd, ls, ld;
+
 void setSpeed(int wheel, int wheel_speed)
 {
   int wheel_direction = HIGH;
@@ -16,11 +25,15 @@ void setSpeed(int wheel, int wheel_speed)
   
   if(wheel == LEFT)
   {
+    ld = wheel_direction;
+    ls = relative_speed;
     digitalWrite(LEFT_DIRECTION, wheel_direction);
     analogWrite(LEFT_SPEED,relative_speed);
   }
   else
   {
+    rd = wheel_direction;
+    rs = relative_speed;
     digitalWrite(RIGHT_DIRECTION, wheel_direction);
     analogWrite(RIGHT_SPEED,relative_speed);
   }
@@ -36,29 +49,77 @@ void init_wheel()
     // Vitesse moteur gauche
     pinMode(RIGHT_SPEED, OUTPUT);
     // Direction moteur gauche
-    pinMode(RIGHT_RIRECTION, OUTPUT);  
+    pinMode(RIGHT_DIRECTION, OUTPUT);  
+}
+
+void parse(char * buffer)
+{
+  int right_speed, left_speed, ret;
+  ret = sscanf(buffer, "set %d %d", &right_speed, &left_speed);
+  if(ret == 2)
+  {
+    setSpeed(LEFT, left_speed);
+    setSpeed(RIGHT, right_speed);
+    Serial.println("Speed set");
+  }
+  else
+  {
+    if(strcmp(buffer, "get") == 0)
+      Serial.println("Speed get");
+      Serial.print("Right: direction=");
+      Serial.print(rd);
+      Serial.print(" speed=");
+      Serial.println(rs);
+      
+      Serial.print("Left: direction=");
+      Serial.print(ld);
+      Serial.print(" speed=");
+      Serial.println(ls);
+  }
+  
+  
 }
 
 void setup()  
 {
   // Open serial communications and wait for port to open:
   Serial.begin(57600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
-    
+   Serial.setTimeout(20000);
+  
    init_wheel();
-}
-
 
   Serial.println("Goodnight moon!");
 }
 
 void loop() // run over and over
 {
-  char c;
-  if(Serial.available())
-    {
+   char c; 
+ 
+   if(Serial.available())
+   {
       c = Serial.read();
       Serial.print(c);
-    }
+      if( buffer_lenght < SIZE_BUFFER -1)
+      {
+        buffer[buffer_lenght++] = c;
+        if(c == '\r')
+        {
+          Serial.println();
+          Serial.println("Execute command");
+          buffer[buffer_lenght] = '\0';
+           parse(buffer);
+           buffer_lenght = 0;
+        }
+      }
+      else
+      {
+          Serial.println();
+          Serial.println("Buffer overflow");
+         buffer_lenght = 0; 
+      }
+      
+      
+   }
+   
+    
 }
