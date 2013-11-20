ee8011667a1e8145acf1c40c54b9a8b7b106cf28
Thibaud Lambert <thibaud.lambert@gmail.com>
2013-11-20 12:11:41 +0100

    Update communication read

--- SerialCom/atmega8/communication.h
+++ SerialCom/atmega8/communication.h
@@ -6,5 +6,7 @@
 void init_com();
 void send_msg(char * msg, int size);
 int recv_msg(char * buffer, int size);
+int is_ready_to_read();
+int is_ready_to_send();
 
 #endif
--- SerialCom/atmega8/serialCom.c
+++ SerialCom/atmega8/serialCom.c
@@ -2,13 +2,20 @@
 #include <util/delay.h>
 #include "communication.h"
 
+#define BUFFER_SIZE 32
+
 int main()
 {
 	char * s = "Hello World!\n";	
+	char buffer[BUFFER_SIZE];
+	int size;
 	init_com();
 	while(1)
 	{
-		send_msg(s,13);
-		_delay_ms(1000.);
+		while(!is_ready_to_read()){};		
+		size = recv_msg(buffer,BUFFER_SIZE);				
+		send_msg(buffer,size);
+		//send_msg(s,13);
+		//_delay_ms(1000.);
 	}
 }
