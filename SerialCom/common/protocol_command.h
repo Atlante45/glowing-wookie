f42285b1d59e6523f230d93e204f99e09e6bf79b
atlante45 <clement.brisset@gmail.com>
2013-11-27 15:38:07 +0100

    Completed command enum

--- SerialCom/common/protocol_command.h
+++ SerialCom/common/protocol_command.h
@@ -2,13 +2,17 @@
 #define PROTOCOL_COMMANDS_H_
 
 enum command {
-  GET_CAPS = 0,
-  RESET,
-  PING,
-  READ,
-  WRITE,
-  GET_TYPE,
-  ETC
+    GET_CAPS      = 0,
+    RESET         = 1,
+    PING          = 2,
+    READ          = 3,
+    WRITE         = 4,
+    GET_TYPE      = 5,
+    SET_TYPE      = 6,
+    GET_FAIL_SAFE = 7,
+    SET_FAIL_SAFE = 8,
+
+    RESERVED      = 15
 };
 
 #endif
