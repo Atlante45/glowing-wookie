0800b153b299067deaa064d16376054dd2e3288e
atlante45 <clement.brisset@gmail.com>
2013-12-17 21:32:52 +0100

    Implemented some TODOs (1 left)  + Changed inconsistant size/length  + Added some protocol state variables

--- SerialCom/raspberry/protocol.cpp
+++ SerialCom/raspberry/protocol.cpp
@@ -1,23 +1,25 @@
 #include "protocol.h"
 
 #include "../common/bits.h"
-#include "../common/checksum.h"
 #include "../common/protocol_util.h"
 
+#include <time.h>
 #include <iostream>
 
 
 Protocol::Protocol(serialib *serialPort){
   port = serialPort;
-  timeout=1000;
+  timeout = 1000;
+  current_reply_id = 0;
+  nb_pins = 0;
 }
 
-int Protocol::parse(int &command, int &reply_code, int &payload_size, char **payload){
+int Protocol::parse(int &command, int &reply_code, int &payload_length, char **payload){
   int read;
 
   // HEADER
   char header[HEADER_LENGTH];
-  read=port->Read(&(header[0]), HEADER_LENGTH, timeout);
+  read = port->Read(&(header[0]), HEADER_LENGTH, timeout);
   if (read!=1){
     std::cout << "[ERROR] While parsing command: couldn't read header" << std::endl;
     return read;
@@ -30,32 +32,36 @@ int Protocol::parse(int &command, int &reply_code, int &payload_size, char **pay
 
   // SIZE
   char size_buffer[DATA_SIZE_LENGTH];
-  read=port->Read(&(size_buffer[0]), DATA_SIZE_LENGTH, timeout);
+  read = port->Read(&(size_buffer[0]), DATA_SIZE_LENGTH, timeout);
   if (read!=1){
     std::cout << "[ERROR] While parsing command: couldn't read packet size" << std::endl;
     return read;
   }
-  int size = binary_read(&(size_buffer[0]), DATA_SIZE_INDEX, DATA_SIZE_SIZE);
-  std::cout << "[DEBUG] packet size=" << size << std::endl;
+
+  int length = binary_read(&(size_buffer[0]), DATA_SIZE_INDEX, DATA_SIZE_SIZE);
+  std::cout << "[DEBUG] packet length=" << length << std::endl;
 
   // REPLYID
   int reply_id = 0;
-  read=port->Read((char*) &reply_id, REPLY_ID_LENGTH, timeout);
+  read = port->Read((char*) &reply_id, REPLY_ID_LENGTH, timeout);
   if (read!=1){
     std::cout << "[ERROR] While parsing command: couldn't read reply_id" << std::endl;
     return read;
   }
-  // TODO check replyid
+  if (reply_id != current_reply_id + 1 || (reply_id < current_reply_id && reply_id != 16)) {
+      std::cout << "[ERROR] While parsing command: invalid reply ID." << std::endl;
+      return INVALID_REPLY_ID;
+  }
   std::cout << "[DEBUG] reply_id=" << reply_id << std::endl;
 
 
   // REPLY SPECIFIC PAYLOAD
-  payload_size = size - HEADER_LENGTH - DATA_SIZE_LENGTH - REPLY_ID_LENGTH - CHECKSUM_LENGTH;
-  std::cout << "[DEBUG] reply specific payload size=" << payload_size << std::endl;
-  if (payload_size < 0)
+  payload_length = length - HEADER_LENGTH - DATA_SIZE_LENGTH - REPLY_ID_LENGTH - CHECKSUM_LENGTH;
+  std::cout << "[DEBUG] reply specific payload length=" << payload_length << std::endl;
+  if (payload_length < 0)
     return -3;
-  *payload = new char[payload_size];
-  read=port->Read(*payload, payload_size, timeout);
+  *payload = new char[payload_length];
+  read = port->Read(*payload, payload_length, timeout);
   if (read!=1){
     std::cout << "[ERROR] While parsing command: couldn't read payload" << std::endl;
     delete[] *payload;
@@ -64,12 +70,21 @@ int Protocol::parse(int &command, int &reply_code, int &payload_size, char **pay
 
   // CHECKSUM
   int checksum = 0;
-  read=port->Read((char*) &checksum, CHECKSUM_LENGTH, timeout);
-  if (read!=1){ // TODO checksum
-    std::cout << "[ERROR] While parsing command: couldn't read checksum" << std::endl;
-    delete[] *payload;
-    return read;
+  read = port->Read((char*) &checksum, CHECKSUM_LENGTH, timeout);
+  if (read!=1){
+      std::cout << "[ERROR] While parsing command: couldn't read checksum" << std::endl;
+      delete[] *payload;
+      return read;
   }
+
+  int packet_length = 0;
+  char *buffer = protocol__make_packet(&packet_length, *header, *payload, payload_length);
+  if (checksum != buffer[packet_length - 1]) {
+      std::cout << "[ERROR] While parsing command: invalid checksum." << std::endl;
+      delete[] buffer;
+      return INVALID_CHECKSUM;
+  }
+  delete[] buffer;
 
   std::cout << "[DEBUG] checksum=" << checksum << std::endl;
 
@@ -79,9 +94,21 @@ int Protocol::parse(int &command, int &reply_code, int &payload_size, char **pay
 }
 
 
-void Protocol::receiveCommand(int &command, int &reply_code, int &payload_size, char **payload){
-  //TODO timeout
-  while(parse(command, reply_code, payload_size, payload)!=0);
+void Protocol::receiveCommand(int &command, int &reply_code, int &payload_length, char **payload){
+    struct timeval start_time, current_time;
+    gettimeofday(&start_time, NULL);
+
+    while(parse(command, reply_code, payload_length, payload)!=0) {
+        gettimeofday(&current_time, NULL);
+        if ((current_time.tv_usec - start_time.tv_usec) / 1000. +
+                (current_time.tv_sec - start_time.tv_sec) * 1000. > timeout) {
+            return;
+        }
+    }
+}
+
+void Protocol::resetProtocolState() {
+    current_reply_id = 0;
 }
 
 void Protocol::sendCommand( char header, char *payload, int payload_length){
@@ -100,12 +127,12 @@ void Protocol::sendCommand( char header, char *payload, int payload_length){
 
 //////////////////////////////// SYNC /////////////////////////////////////
 
-int Protocol::getCaps(int &output__nb_pins){
+int Protocol::getCaps(int &output__nb_pins, mask_t *output__pins_type){
   sendGetCaps();
 
-  int command, reply_code, payload_size;
+  int command, reply_code, payload_length;
   char *payload = NULL;
-  receiveCommand(command, reply_code, payload_size, &payload);
+  receiveCommand(command, reply_code, payload_length, &payload);
 
   if (payload == NULL)
     return -1;
@@ -115,7 +142,7 @@ int Protocol::getCaps(int &output__nb_pins){
   }
 
   output__nb_pins = binary_read(payload, 0, NB_PINS_SIZE);
-  //TODO read type_mask
+  output__pins_type = mask__from_string(payload, output__nb_pins, 3, NB_PINS_SIZE);
 
   delete[] payload;
 
@@ -125,9 +152,9 @@ int Protocol::getCaps(int &output__nb_pins){
 int Protocol::reset(){
   sendReset();
 
-  int command, reply_code, payload_size;
+  int command, reply_code, payload_length;
   char *payload = NULL;
-  receiveCommand(command, reply_code, payload_size, &payload);
+  receiveCommand(command, reply_code, payload_length, &payload);
   
   if (payload == NULL)
     return -1;
@@ -136,18 +163,19 @@ int Protocol::reset(){
     return -2;
   }
 
+  resetProtocolState();
+
   delete payload;
 
   return 0;
-
 }
 
 int Protocol::ping(int &output__protocol_version){
   sendPing();
 
-  int command, reply_code, payload_size;
+  int command, reply_code, payload_length;
   char *payload = NULL;
-  receiveCommand(command, reply_code, payload_size, &payload);
+  receiveCommand(command, reply_code, payload_length, &payload);
   
   if (payload == NULL)
     return -1;
@@ -166,9 +194,9 @@ int Protocol::ping(int &output__protocol_version){
 int Protocol::read(enum types type, mask_t *pins, mask_t *output__values){ 
   sendRead(type, pins);
   
-  int command, reply_code, payload_size;
+  int command, reply_code, payload_length;
   char *payload = NULL;
-  receiveCommand(command, reply_code, payload_size, &payload);
+  receiveCommand(command, reply_code, payload_length, &payload);
   
   if (payload == NULL)
     return -1;
@@ -177,7 +205,7 @@ int Protocol::read(enum types type, mask_t *pins, mask_t *output__values){
     return -2;
   }
 
-  int nb_values = payload_size / TYPE_SIZE[type];
+  int nb_values = payload_length * 8 / TYPE_SIZE[type];
   output__values = mask__from_string(payload, nb_values, TYPE_SIZE[type], 0);
   
   delete[] payload;
@@ -188,9 +216,9 @@ int Protocol::read(enum types type, mask_t *pins, mask_t *output__values){
 int Protocol::write(enum types type, mask_t *pins, mask_t *values){ 
   sendWrite(type, pins, values);
 
-  int command, reply_code, payload_size;
+  int command, reply_code, payload_length;
   char *payload = NULL;
-  receiveCommand(command, reply_code, payload_size, &payload);
+  receiveCommand(command, reply_code, payload_length, &payload);
   
   if (payload == NULL)
     return -1;
@@ -207,9 +235,9 @@ int Protocol::write(enum types type, mask_t *pins, mask_t *values){
 int Protocol::setType(mask_t *pins, mask_t *states){ 
   sendSetType(pins, states);
 
-  int command, reply_code, payload_size;
+  int command, reply_code, payload_length;
   char *payload = NULL;
-  receiveCommand(command, reply_code, payload_size, &payload);
+  receiveCommand(command, reply_code, payload_length, &payload);
   
   if (payload == NULL)
     return -1;
@@ -226,9 +254,9 @@ int Protocol::setType(mask_t *pins, mask_t *states){
 int Protocol::getType(mask_t *pins, mask_t *output__type_mask){ 
   sendGetType(pins);
   
-  int command, reply_code, payload_size;
+  int command, reply_code, payload_length;
   char *payload = NULL;
-  receiveCommand(command, reply_code, payload_size, &payload);
+  receiveCommand(command, reply_code, payload_length, &payload);
   
   if (payload == NULL)
     return -1;
@@ -237,7 +265,7 @@ int Protocol::getType(mask_t *pins, mask_t *output__type_mask){
     return -2;
   }
 
-  int nb_values = payload_size / TYPE_DATA_SIZE;
+  int nb_values = payload_length * 8 / TYPE_DATA_SIZE;
   output__type_mask = mask__from_string(payload, nb_values, TYPE_DATA_SIZE, 0);
   
   delete[] payload;
@@ -246,18 +274,35 @@ int Protocol::getType(mask_t *pins, mask_t *output__type_mask){
 
 }
 
-void Protocol::getFailSafe(mask_t *pins){ 
+int Protocol::getFailSafe(mask_t *pins){
   sendGetFailSafe(pins);
-  //TODO
 
+  int command, reply_code, payload_length;
+  char *payload = NULL;
+  receiveCommand(command, reply_code, payload_length, &payload);
+
+  if (payload == NULL)
+    return -1;
+  if (command != GET_FAIL_SAFE || (reply_code != SUCCESS && reply_code != PARTIAL_SUCCESS)){
+    delete payload;
+    return -2;
+  }
+
+  for (int i = 0; i < nb_pins; ++i) {
+      // TODO
+  }
+
+  delete[] payload;
+
+  return 0;
 }
 
 int Protocol::setFailSafe(int timeout, enum types type, mask_t *pins, mask_t *values){
   sendSetFailSafe(timeout, type, pins, values);
   
-  int command, reply_code, payload_size;
+  int command, reply_code, payload_length;
   char *payload = NULL;
-  receiveCommand(command, reply_code, payload_size, &payload);
+  receiveCommand(command, reply_code, payload_length, &payload);
   
   if (payload == NULL)
     return -1;
@@ -506,9 +551,9 @@ int main () {
   ////////////////////////////////
 
 
-  int command, reply_code, payload_size;
+  int command, reply_code, payload_length;
   char *payload = NULL;
-  p.receiveCommand(command, reply_code, payload_size, &payload);
+  p.receiveCommand(command, reply_code, payload_length, &payload);
   
   std::cout << "Received payload: " << (int) payload[0];
   std::cout << std::endl << std::endl;
